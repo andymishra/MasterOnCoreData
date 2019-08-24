b051b4e05bdffb4f921f6f1ccd7b9960d922b0a0
Anand Mishra <>
2019-08-24 13:55:12 +0530

    Initial Setup

---
+++ MasterOnCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterOnCoreData
+//
+//  Created by Anand Mishra on 24/08/19.
+//  Copyright © 2019 Anand Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
